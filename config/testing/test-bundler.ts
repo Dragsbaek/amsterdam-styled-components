dad84f92b61dcfbf79e43b386694baa42eaa094f
Jon Koops <jonkoops@gmail.com>
2020-11-05 18:34:40 +0100

    Upgrade to React 17

--- config/testing/test-bundler.ts
+++ config/testing/test-bundler.ts
@@ -1,6 +1,8 @@
 // (ES7 generator support is required by redux-
 import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+// TODO: Replace this with the offical adapter once released
+// See: https://github.com/enzymejs/enzyme/issues/2429
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 
 import 'jest-styled-components'
 import '@testing-library/jest-dom'
